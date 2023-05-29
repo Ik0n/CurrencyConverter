bd60cf49579924fd61d9cddb6fbfa107988cae68
Ik0n <Ik0nChannel1@mail.ru>
2023-05-29 13:25:48 +0300

    Add CurrencyRate screen

---
+++ app/src/main/java/ru/ikon/currencyconverter/ui/adapters/CurrencyRateAdapter.kt
@@ -0,0 +1,29 @@
+package ru.ikon.currencyconverter.ui.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item_currency_rate.view.*
+import ru.ikon.currencyconverter.R
+
+class CurrencyRateAdapter(var rates: Map<String, Double>) : RecyclerView.Adapter<CurrencyRateAdapter.ViewHolder>() {
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(code: String, rate: Double) {
+            itemView.tv_currency_code.text = code
+            itemView.tv_currency_rate.text = rate.toString()
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_currency_rate, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(rates.keys.elementAt(position), rates.values.elementAt(position))
+    }
+
+    override fun getItemCount(): Int = rates.size
+
+}
